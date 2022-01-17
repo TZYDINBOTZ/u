2cd6213071d9f04608fb4366a33a6851d4a746b5
TZYDINBOTZ <97898960+TZYDINBOTZ@users.noreply.github.com>
2022-01-18 03:48:37 +0700

    Update index.js

--- diamond/index.js
+++ diamond/index.js
@@ -1,3 +1,3 @@
 exports.mm_mb = require('./mm_mb')
-exports.list_diamond = require('./list_diamond')
+exports.list_diamond = require('./list_minta owner')
 exports.list_harga_diamond = require('./list_harga_diamond')
